27f629cbf9940d040cb0f22226156113ef862dbd
Code Differently <codedifferently@Codes-MacBook-Air.local>
2022-05-18 16:57:48 -0400

    updates to algos

--- src/main/java/algos/codeprep03/PickingTickets.java
+++ src/main/java/algos/codeprep03/PickingTickets.java
@@ -9,59 +9,38 @@ public class PickingTickets {
     The function is expected to return an INTEGER.
     The function accepts INTEGER_ARRAY ticket as parameter.
      */
+    public int map(){
+        maxTickets();
+        return 0;
+    }
 
     public static int maxTickets(List<Integer> tickets){
         // return type int
         // determine the maximum length of a subsequence chosen from the tickets array
 
         // tickets = [8,5,4,8,4]
+        Collections.sort(tickets);
+        // tickets = [4,4,5,8,8]
 
-//        Collections.sort(tickets);
-//
-//
-//        HashMap<Integer, ArrayList<Integer>> sequencesMap = new HashMap<>();
-//
-//        ArrayList<Integer> subArray = new ArrayList<>();
-//        sequencesMap.put(tickets.get(0), subArray);
-//        subArray.add(tickets.get(0));
-//
-//        for (int i = 1; i < tickets.size()-2; i++){
-//            for (int j = i+1; j < tickets.size(); j++) {
-//                int currentNum = tickets.get(j-1);
-//                int nextNum = tickets.get(j);
-//
-//                if (nextNum - currentNum <= 1){
-//                    subArray.add(currentNum);
-//                    subArray.add(nextNum);
-//                } else {
-//                    subArray = new ArrayList<>();
-//                    sequencesMap.put(currentNum, subArray);
-//                }
-//            }
-//        }
-//
-//        System.out.println(subArray);
-//        System.out.println(sequencesMap);
-//
-//        Map.Entry<Integer, ArrayList<Integer>> maxEntry = null;
-//
-//        for (Map.Entry<Integer, ArrayList<Integer>> entry : sequencesMap.entrySet()){
-//            Integer entryValue = entry.getValue().size();
-//            String f = "";
-//
-//        }
-        int frequency[] = new int[101];
-        int result = Integer.MIN_VALUE;
+        // at minimum subsequence will be 1 because there will be a starting number to compare other numbers with
+        int subsequence = 1;
+        // this wil keep count of the largest unbroken range of integers
+        int maxSequence = 0;
 
-        for (int i = 0; i < tickets.size(); i++) {
-            int index=tickets.get(i);
-            frequency[index]++; //frequency[index]=frequency[index]+1
+        // iterate through the tickets list
+        for (int i = 0; i < tickets.size()-1; i++){
+            Integer currentNum = tickets.get(i);
+            Integer nextNum = tickets.get(i+1);
+            Integer nextValue = tickets.get(i)+1;
+
+            if (currentNum.equals(nextNum) || nextValue.equals(nextNum) ){
+                subsequence++;
+            } else {
+                maxSequence = Math.max(maxSequence, subsequence);
+                subsequence = 1;
+            }
         }
 
-        for (int i = 1; i <= 100; i++) {
-            result = Math.max(result, frequency[i] + frequency[i - 1]);
-        }
-        return result;
-
+        return maxSequence;
     }
 }
--- src/test/java/algos/codeprep03/PickingTicketsTest.java
+++ src/test/java/algos/codeprep03/PickingTicketsTest.java
@@ -1,25 +1,21 @@
 package algos.codeprep03;
 
+import static algos.codeprep03.PickingTickets.maxTickets;
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class PickingTicketsTest {
-    PickingTickets pickingTickets;
 
-    @BeforeEach
-    public void setup(){
-        pickingTickets = new PickingTickets();
-    }
+    String
 
     @Test
     public void testOne(){
         List<Integer> input = Arrays.asList(8, 5, 4, 8, 4);
         int expected = 3;
-        int actual = pickingTickets.maxTickets(input);
+        int actual = maxTickets(input);
         assertEquals(expected,actual);
     }
 
@@ -27,7 +23,7 @@ public class PickingTicketsTest {
     public void testTwo(){
         List<Integer> input = Arrays.asList(1,3,2,4,6,7,8); //1,2,3,4,6,7,8
         int expected = 4;
-        int actual = pickingTickets.maxTickets(input);
+        int actual = maxTickets(input);
         assertEquals(expected,actual);
     }
 
@@ -35,7 +31,7 @@ public class PickingTicketsTest {
     public void testThree(){
         List<Integer> input = Arrays.asList(9,5,6,4,8);
         int expected = 3;
-        int actual = pickingTickets.maxTickets(input);
+        int actual = maxTickets(input);
         assertEquals(expected,actual);
     }
 }
